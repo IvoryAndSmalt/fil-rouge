e72982a36eed8112513108156d1b9edbcb1f2c15
Lucas <lucas180395@gmail.com>
2018-12-09 01:06:23 +0100

    ajout logo et autres images

--- Views/footer.php
+++ Views/footer.php
@@ -1,10 +1,30 @@
 <br> da footer
 <footer id="footer" class="bg-ivory">
     <ul class="bannerul">
-        <li class="footerli thin"><a href="#header" id="accueil-link">Accueil</a></li>
-        <li class="footerli thin"><a href="#header" id="accueil-link">CV</a></li>
-        <li class="footerli thin"><a href="#header" id="accueil-link">Projets</a></li>
-        <li class="footerli thin"><a href="#to-form" id="accueil-link">@</a></li>
+        <li class="footerli thin">
+            <a href="#header" id="accueil-link">
+                <img class="icon" src="assets/img/home.svg" alt="Accueil">
+                Accueil
+            </a>
+        </li>
+        <li class="footerli thin">
+            <a href="#header" id="accueil-link">
+                <img class="icon" src="assets/img/contact.svg" alt="CV">
+                CV
+            </a>
+        </li>
+        <li class="footerli thin">
+            <a href="#header" id="accueil-link">
+                <img class="icon" src="assets/img/home.svg" alt="Projets">
+                Projets
+            </a>
+        </li>
+        <li class="footerli thin">
+            <a href="#to-form" id="accueil-link">
+                <img class="icon" src="assets/img/contact.svg" alt="Contact">
+                Contact
+            </a>
+        </li>
     </ul>
 </footer>
 
